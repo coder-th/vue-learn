7179d13bc09a03f341514aa10415b91a5c601ec8
天衡 <13265317640@163.com>
2021-07-26 11:10:28 +0800

    feat: 增加一些判断工具

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,10 +1,21 @@
-import { isObject } from "@vue/shared";
+import { def, isObject, toRawType } from "@vue/shared";
 import {
   mutableHandlers,
   shallowReactiveHandlers,
   readonlyHandlers,
   shallowReadonlyHandlers,
 } from "./baseHandlers";
+export const enum ReactiveFlags {
+  SKIP = "__v_skip",
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+  RAW = "__v_raw",
+}
+const enum TargetType {
+  INVALID = 0,
+  COMMON = 1,
+  COLLECTION = 2,
+}
 /**
  * 用来存储需要响应式的对象
  */
@@ -16,17 +27,34 @@ const readonlyMap = new WeakMap();
 /**
  * 创建 一个响应式代理,将结果进行缓存
  * @param target 目标对象
- * @param isReadOnly 是否是只读的
+ * @param isReadonly 是否是只读的
  * @param baseHandlers 拦截器
  * @returns 返回代理后的对象
  */
-function createReactiveObject(target, isReadOnly, baseHandlers) {
+function createReactiveObject(target, isReadonly, baseHandlers) {
   // 用户传入的值不是对象，就不做了
   if (!isObject(target)) return;
+  // ① 源数据为Proxy
+  // __v_raw仅当数据源已经是Proxy时才会有，既然是Proxy说明是转化过的响应式数据，
+  // 那么直接把对象原封不动的返回即可
+  if (
+    target[ReactiveFlags.RAW] &&
+    !(isReadonly && target[ReactiveFlags.IS_REACTIVE])
+  ) {
+    return target;
+  }
+  // 2 源数据合法性白名单检测
+  // 只有target在可转化类型白名单里才会进行转化，否则直接返回target
+  // canObserve会校验target的__v_skip是否为true、target类型是否在白名单里、target是否
+  // 为冻结对象，三者同时满足才会继续转化
+  const targetType = getTargetType(target)
+  if (targetType === TargetType.INVALID) {
+    return target
+  }
   /**
    * 当前的存储器
    */
-  const proxyMap = isReadOnly ? readonlyMap : reactiveMap;
+  const proxyMap = isReadonly ? readonlyMap : reactiveMap;
   // 取出当前对象的代理
   const existProxy = proxyMap.get(target);
   if (existProxy) {
@@ -69,4 +97,78 @@ export function readonly(target) {
  */
 export function shallowReadonly(target) {
   return createReactiveObject(target, true, shallowReadonlyHandlers);
+}
+/**
+ * 类型映射表
+ * @param rawType
+ * @returns
+ */
+function targetTypeMap(rawType: string) {
+  switch (rawType) {
+    case "Object":
+    case "Array":
+      return TargetType.COMMON;
+    case "Map":
+    case "Set":
+    case "WeakMap":
+    case "WeakSet":
+      return TargetType.COLLECTION;
+    default:
+      return TargetType.INVALID;
+  }
+}
+/**
+ * 获取当前对象分类
+ * @param target
+ * @returns
+ */
+export function getTargetType(target) {
+  // 当前对象被标记为原生对象或者，该对象不可被扩展，说明该对象无效，不需要在进行响应式代理
+  return target[ReactiveFlags.SKIP] || !Object.isExtensible(target)
+    ? TargetType.INVALID
+    : targetTypeMap(toRawType(target));
+}
+/**
+ * 判断当前对象是不是可响应的
+ * @param value
+ * @returns
+ */
+export function isReactive(value: unknown): boolean {
+  if (isReadonly(value)) {
+    return isReactive(value[ReactiveFlags.RAW]);
+  }
+  return !!(value && value[ReactiveFlags.IS_REACTIVE]);
+}
+/**
+ * 判断当前对象是不是只读
+ * @param value
+ * @returns
+ */
+export function isReadonly(value: unknown): boolean {
+  return !!(value && value[ReactiveFlags.IS_READONLY]);
+}
+/**
+ * 判断当前对象有没有被代理过
+ * @param value
+ * @returns
+ */
+export function isProxy(value: unknown): boolean {
+  return isReactive(value) || isReadonly(value);
+}
+/**
+ * 得到原生对象
+ * @param observed
+ * @returns
+ */
+export function toRaw<T>(observed: T): T {
+  return (observed && toRaw(observed[ReactiveFlags.RAW])) || observed;
+}
+/**
+ * 标记该对象为原生对象，直接跳过响应式代理
+ * @param value
+ * @returns
+ */
+export function markRaw<T extends object>(value: T): T {
+  def(value, ReactiveFlags.SKIP, true);
+  return value;
 }
